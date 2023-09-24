166941a9405f14211d45178af403282d5afb6fb0
Elia Cunial <elia.cunial@gmail.com>
2023-09-24 20:43:02 +0200

    Nerd fonts update

--- install.sh
+++ install.sh
@@ -1,28 +1,17 @@
-#!/bin/bash
+d!/bin/bash
 
 fontName="SourceCodePro"
 fontDir="$HOME/.local/share/fonts"
 
 downloadFonts() {
-	echo "[-] Download fonts [-]"
-	echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$fontName.zip"
-	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/"$fontName".zip
-	mkdir -p "$fontDir"
-	unzip "$fontName".zip -d "$fontDir"
-	fc-cache -fv
-	rm "$fontName".zip
-	echo "done!"
-}
-
-createLinks() {
-	for f in .config/*; do
-		[ -e "$HOME/$f" ] && rm "$HOME/$f"
-		ln -s "$PWD/$f" "$HOME/$f"
-	done
-	for f in .*; do
-		[ ! -d "$f" ] && ln -s "$PWD/$f" "$HOME/$f"
-	done
-
+  echo "[-] Download fonts [-]"
+  echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/$fontName.zip"
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/"$fontName".zip
+  mkdir -p "$fontDir"
+  unzip "$fontName".zip -d "$fontDir"
+  fc-cache -fv
+  rm "$fontName".zip
+  echo "done!"
 }
 
 downloadFonts
