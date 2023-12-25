eb2b64e4591c0b9b6ea214ba96eb70e0693de238
Elia Cunial <elia.cunial@gmail.com>
2023-12-25 21:37:35 +0100

    better cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,71 +3,39 @@ return {
 	init = function()
 		vim.o.completeopt = "menu,menuone,noselect"
 	end,
-	config = function()
-		local has_words_before = function()
-			local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
-
-		local feedkey = function(key, mode)
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-		end
+	event = "InsertEnter",
+	opts = function()
+		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 		local cmp = require("cmp")
+		local defaults = require("cmp.config.default")()
 		local lspkind = require("lspkind")
-
-		cmp.setup({
-			enabled = function()
-				-- disable completion in comments
-				local context = require("cmp.config.context")
-				-- keep command mode completion enabled when cursor is in a comment
-				if vim.api.nvim_get_mode().mode == "c" then
-					return true
-				else
-					return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-				end
-			end,
-			window = {
-				documentation = cmp.config.window.bordered(),
+		return {
+			completion = {
+				completeopt = "menu,menuone,noinsert",
 			},
 			snippet = {
 				expand = function(args)
-					vim.fn["vsnip#anonymous"](args.body)
+					require("luasnip").lsp_expand(args.body)
 				end,
 			},
-			formatting = {
-				format = lspkind.cmp_format({
-					with_text = true,
-					maxwidth = 50,
-					menu = { buffer = "[  ]", nvim_lsp = "[ 󰅩 ]", vsnip = "[  ]" },
-				}),
-			},
-			mapping = {
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-o>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif vim.fn["vsnip#available"]() == 1 then
-						feedkey("<Plug>(vsnip-expand-or-jump)", "")
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-					end
-				end, { "i", "s" }),
-
-				["<S-Tab>"] = cmp.mapping(function()
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-						feedkey("<Plug>(vsnip-jump-prev)", "")
-					end
-				end, { "i", "s" }),
-			},
-			sources = {
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<S-CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<C-CR>"] = function(fallback)
+					cmp.abort()
+					fallback()
+				end,
+			}),
+			sources = cmp.config.sources({
 				{ name = "nvim_lsp_signature_help" },
 				{ name = "nvim_lsp" },
 				{
@@ -81,35 +49,45 @@ return {
 					name = "buffer",
 					entry_filter = function()
 						local context = require("cmp.config.context")
-						return context.in_treesitter_capture("string") and not context.in_syntax_group("String")
+						return context.in_treesitter_capture("string") or context.in_syntax_group("String")
 					end,
 				},
 				{
 					name = "path",
 					entry_filter = function()
 						local context = require("cmp.config.context")
-						return context.in_treesitter_capture("string") and not context.in_syntax_group("String")
+						return context.in_treesitter_capture("string") or context.in_syntax_group("String")
 					end,
 				},
+			}),
+			formatting = {
+				format = lspkind.cmp_format({
+					with_text = true,
+					maxwidth = 50,
+					menu = { buffer = "[  ]", nvim_lsp = "[ 󰅩 ]", vsnip = "[  ]" },
+				}),
 			},
-		})
+			experimental = {
+				ghost_text = {
+					hl_group = "CmpGhostText",
+				},
+			},
+			sorting = defaults.sorting,
+		}
+	end,
+	---@param opts cmp.ConfigSchema
+	config = function(_, opts)
+		for _, source in ipairs(opts.sources) do
+			source.group_index = source.group_index or 1
+		end
+		require("cmp").setup(opts)
 	end,
 	dependencies = {
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{ "hrsh7th/cmp-buffer" },
 		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-vsnip" },
+		"saadparwaiz1/cmp_luasnip",
 		{ "onsails/lspkind-nvim" },
-		{
-			"hrsh7th/vim-vsnip",
-			init = function()
-				vim.g.vsnip_snippet_dir = "~/.config/nvim/.vsnip/"
-			end,
-			dependencies = {
-				{ "hrsh7th/vim-vsnip-integ" },
-				{ "rafamadriz/friendly-snippets" },
-			},
-		},
 		{
 			"ray-x/lsp_signature.nvim",
 			config = function()
