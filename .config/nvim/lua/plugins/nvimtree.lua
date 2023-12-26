d6b2f77cb549505f8367a5bbbfbfeb47efe02fee
Elia Cunial <elia.cunial@gmail.com>
2023-12-26 12:27:13 +0100

    floating nevimtree

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -1,3 +1,7 @@
+local gwidth = vim.api.nvim_list_uis()[1].width
+local gheight = vim.api.nvim_list_uis()[1].height
+local width = 60
+local height = 20
 return {
 	"nvim-tree/nvim-tree.lua",
 	cmd = "NvimTreeToggle",
@@ -10,7 +14,19 @@ return {
 		sort = {
 			sorter = "case_sensitive",
 		},
-		view = { width = 25, side = "left" },
+		view = {
+			width = 25,
+			float = {
+				enable = true,
+				open_win_config = {
+					relative = "editor",
+					width = width,
+					height = height,
+					row = (gheight - height) * 0.4,
+					col = (gwidth - width) * 0.5,
+				},
+			},
+		},
 		renderer = {
 			indent_markers = { enable = true, icons = { corner = "└ ", edge = "│ ", none = "  " } },
 		},
