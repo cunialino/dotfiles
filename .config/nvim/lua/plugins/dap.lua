346d469c023c611bd4d2b66bbd0f2042f4101f86
Elia Cunial <elia.cunial@gmail.com>
2023-12-26 14:54:06 +0100

    load corretly

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,5 +1,6 @@
 return {
 	"jay-babu/mason-nvim-dap.nvim",
+	ft = { "python" },
 	dependencies = {
 		{
 			"mfussenegger/nvim-dap",
@@ -16,4 +17,20 @@ return {
 		ensure_installed = { "debugpy" },
 		handlers = {},
 	},
+	keys = function()
+		local wk = require("which-key")
+		local keys = {
+			["<leader>"] = {
+				d = {
+					name = "Debug",
+					b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+					c = { "<cmd>:lua require'dap'.continue()<cr>", "Continue debug" },
+					o = { "<cmd>:lua require'dap'.step_over()<cr>", "Step over" },
+					i = { "<cmd>:lua require'dap'.step_into()<cr>", "Step into" },
+					t = { "<cmd>:lua require('dapui').toggle()<cr>", "Toggle ui" },
+				},
+			},
+		}
+		wk.register(keys)
+	end,
 }
