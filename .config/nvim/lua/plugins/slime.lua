f6b6602c36134e3798bbc420af7312c8146fd78a
Elia Cunial <elia.cunial@gmail.com>
2024-01-18 23:52:44 +0100

    Trying new repl

---
+++ .config/nvim/lua/plugins/slime.lua
@@ -0,0 +1,10 @@
+return {
+  "jpalardy/vim-slime",
+  config = function ()
+    vim.g.slime_target = "zellij"
+    vim.g.slime_default_config = {
+      session_id="current",
+      relative_pane="down"
+    }
+  end
+}
