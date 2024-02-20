7612569f15ff7aa76f203202addc7cef6ac5b2d8
Elia Cunial <elia.cunial@gmail.com>
2024-02-20 12:12:21 +0100

    Auto folding

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -97,5 +97,7 @@ return {
 			end, opts.ensure_installed)
 		end
 		require("nvim-treesitter.configs").setup(opts)
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 	end,
 }
