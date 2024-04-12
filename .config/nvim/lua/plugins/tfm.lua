d25a465c74f1df85d7399050c66c1ee37410c7a7
Elia Cunial <elia.cunial@gmail.com>
2024-04-12 23:26:32 +0200

    tfm

--- .config/nvim/lua/plugins/tfm.lua
+++ .config/nvim/lua/plugins/tfm.lua
@@ -2,24 +2,9 @@ return {
 	"rolv-apneseth/tfm.nvim",
 	lazy = false,
 	opts = {
-		-- TFM to use
-		-- Possible choices: "ranger" | "nnn" | "lf" | "vifm" | "yazi" (default)
 		file_manager = "yazi",
-		-- Replace netrw entirely
-		-- Default: false
 		replace_netrw = true,
 		enable_cmds = false,
-		-- Custom keybindings only applied within the TFM buffer
-		-- Default: {}
-		keybindings = {
-			["<ESC>"] = "q",
-			-- Override the open mode (i.e. vertical/horizontal split, new tab)
-			-- Tip: you can add an extra `<CR>` to the end of these to immediately open the selected file(s) (assuming the TFM uses `enter` to finalise selection)
-			["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
-			["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
-			["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
-		},
-		-- Customise UI. The below options are the default
 		ui = {
 			border = "rounded",
 			height = 0.75,
@@ -36,14 +21,6 @@ return {
 				require("tfm").open()
 			end,
 			desc = "TFM",
-		},
-		{
-			"<leader>mt",
-			function()
-				local tfm = require("tfm")
-				tfm.open(nil, tfm.OPEN_MODE.tabedit)
-			end,
-			desc = "TFM - new tab",
-		},
+		}
 	},
 }
