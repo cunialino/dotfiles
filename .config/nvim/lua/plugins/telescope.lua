de2bc577e85df68a9c6e0f2ede6feaf92fc688d8
Elia Cunial <elia.cunial@gmail.com>
2024-04-14 00:42:21 +0200

    Format telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,32 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.6',
-  dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  },
-  config = function(_, opts)
-    require("telescope").setup(opts)
-    require('telescope').load_extension('fzf')
-    local builtins = require("telescope.builtin")
-    local wk = require("which-key")
-    local keys = {
-      ["<leader>"] = {
-        t = {
-          name = "Telescope",
-          f = {builtins.find_files, "Files"},
-          g = {builtins.git_files, "Git Files"},
-          b = {builtins.buffers, "buffers"},
-          s = {builtins.current_buffer_fuzzy_find, "Search"},
-          r = {builtins.resume, "Resume"},
-          t = {builtins.treesitter, "Treesitter"},
-          B = {builtins.builtin, "Pickers"},
-        },
-      },
-    }
-    wk.register(keys)
-  end,
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-dap.nvim" },
+	},
+	config = function(_, opts)
+		local ts = require("telescope")
+		ts.setup(opts)
+		ts.load_extension("fzf")
+		ts.load_extension("dap")
+		local builtins = require("telescope.builtin")
+		local wk = require("which-key")
+		local keys = {
+			["<leader>"] = {
+				t = {
+					name = "Telescope",
+					f = { builtins.find_files, "Files" },
+					g = { builtins.git_files, "Git Files" },
+					b = { builtins.buffers, "buffers" },
+					s = { builtins.current_buffer_fuzzy_find, "Search" },
+					r = { builtins.resume, "Resume" },
+					t = { builtins.treesitter, "Treesitter" },
+					B = { builtins.builtin, "Pickers" },
+				},
+			},
+		}
+		wk.register(keys)
+	end,
 }
