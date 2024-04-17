0f011be9611c7d27ef9bc19806d808b297812865
Elia Cunial <elia.cunial@gmail.com>
2024-04-17 23:32:15 +0200

    Better telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,21 +5,68 @@ return {
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		{ "nvim-telescope/telescope-dap.nvim" },
-		{ "nvim-telescope/telescope-live-grep-args.nvim" },
-	},
-	opts = {
-		defaults = {
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-			},
+		{
+			"nvim-telescope/telescope-live-grep-args.nvim",
 		},
 	},
+	opts = function()
+    local actions = require("telescope.actions")
+    local open_with_trouble = require("trouble.providers.telescope").open_with_trouble
+    local open_selected_with_trouble = require("trouble.providers.telescope").open_selected_with_trouble
+
+		local lga_actions = require("telescope-live-grep-args.actions")
+    return {
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        -- open files in the first window that is an actual file.
+        -- use the current window if no other window is available.
+        get_selection_window = function()
+          local wins = vim.api.nvim_list_wins()
+          table.insert(wins, 1, vim.api.nvim_get_current_win())
+          for _, win in ipairs(wins) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].buftype == "" then
+              return win
+            end
+          end
+          return 0
+        end,
+        mappings = {
+          i = {
+            ["<c-t>"] = open_with_trouble,
+            ["<a-t>"] = open_selected_with_trouble,
+            ["<C-Down>"] = actions.cycle_history_next,
+            ["<C-Up>"] = actions.cycle_history_prev,
+            ["<C-f>"] = actions.preview_scrolling_down,
+            ["<C-u>"] = actions.preview_scrolling_up,
+          },
+          n = {
+            ["q"] = actions.close,
+          },
+        },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+      },
+			extensions = {
+				live_grep_args = {
+					auto_quoting = true,
+					mappings = {
+						i = {
+							["<C-k>"] = lga_actions.quote_prompt(),
+						},
+					},
+				},
+			},
+		}
+	end,
 	config = function(_, opts)
 		local ts = require("telescope")
 		ts.setup(opts)
@@ -33,13 +80,14 @@ return {
 				t = {
 					name = "Telescope",
 					f = { builtins.find_files, "Files" },
+					F = { builtins.find_files, "Files" },
 					g = { builtins.git_files, "Git Files" },
 					b = { builtins.buffers, "buffers" },
 					s = { builtins.current_buffer_fuzzy_find, "Search" },
-					r = { builtins.resume, "Resume" },
+					R = { builtins.resume, "Resume" },
 					t = { builtins.treesitter, "Treesitter" },
-					B = { builtins.builtin, "Pickers" },
-					l = { builtins.live_grep, "RipGrep" },
+					P = { builtins.builtin, "Pickers" },
+					r = { require("telescope").extensions.live_grep_args.live_grep_args, "RipGrep" },
 				},
 			},
 		}
