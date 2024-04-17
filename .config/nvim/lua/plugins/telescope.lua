feb17d4cdd21b54a41b49909675c80f3bc8acc61
Elia Cunial <elia.cunial@gmail.com>
2024-04-18 00:07:21 +0200

    Better trouble telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,41 +10,39 @@ return {
 		},
 	},
 	opts = function()
-    local actions = require("telescope.actions")
-    local open_with_trouble = require("trouble.providers.telescope").open_with_trouble
-    local open_selected_with_trouble = require("trouble.providers.telescope").open_selected_with_trouble
+		local actions = require("telescope.actions")
+		local open_with_trouble = require("trouble.sources.telescope").open
 
 		local lga_actions = require("telescope-live-grep-args.actions")
-    return {
-      defaults = {
-        prompt_prefix = " ",
-        selection_caret = " ",
-        -- open files in the first window that is an actual file.
-        -- use the current window if no other window is available.
-        get_selection_window = function()
-          local wins = vim.api.nvim_list_wins()
-          table.insert(wins, 1, vim.api.nvim_get_current_win())
-          for _, win in ipairs(wins) do
-            local buf = vim.api.nvim_win_get_buf(win)
-            if vim.bo[buf].buftype == "" then
-              return win
-            end
-          end
-          return 0
-        end,
-        mappings = {
-          i = {
-            ["<c-t>"] = open_with_trouble,
-            ["<a-t>"] = open_selected_with_trouble,
-            ["<C-Down>"] = actions.cycle_history_next,
-            ["<C-Up>"] = actions.cycle_history_prev,
-            ["<C-f>"] = actions.preview_scrolling_down,
-            ["<C-u>"] = actions.preview_scrolling_up,
-          },
-          n = {
-            ["q"] = actions.close,
-          },
-        },
+		return {
+			defaults = {
+				prompt_prefix = " ",
+				selection_caret = " ",
+				-- open files in the first window that is an actual file.
+				-- use the current window if no other window is available.
+				get_selection_window = function()
+					local wins = vim.api.nvim_list_wins()
+					table.insert(wins, 1, vim.api.nvim_get_current_win())
+					for _, win in ipairs(wins) do
+						local buf = vim.api.nvim_win_get_buf(win)
+						if vim.bo[buf].buftype == "" then
+							return win
+						end
+					end
+					return 0
+				end,
+				mappings = {
+					i = {
+						["<c-t>"] = open_with_trouble,
+						["<C-Down>"] = actions.cycle_history_next,
+						["<C-Up>"] = actions.cycle_history_prev,
+						["<C-f>"] = actions.preview_scrolling_down,
+						["<C-u>"] = actions.preview_scrolling_up,
+					},
+					n = {
+						["q"] = actions.close,
+					},
+				},
 				vimgrep_arguments = {
 					"rg",
 					"--color=never",
@@ -54,7 +52,7 @@ return {
 					"--column",
 					"--smart-case",
 				},
-      },
+			},
 			extensions = {
 				live_grep_args = {
 					auto_quoting = true,
