f14692590697cbb32c41005049b5e3b4a918a332
Elia Cunial <elia.cunial@gmail.com>
2024-05-31 11:26:45 +0200

    Diffview defaults

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,12 @@
 return {
-  "sindrets/diffview.nvim",
+	"sindrets/diffview.nvim",
+	opts = {
+		view = {
+			merge_tool = {
+				layout = "diff3_mixed",
+				disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+				winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+			},
+		},
+	},
 }
