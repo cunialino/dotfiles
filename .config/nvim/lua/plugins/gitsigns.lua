cb62e01bcf9d33d4c6020364f8a13774c686267e
Elia Cunial <elia.cunial@gmail.com>
2024-06-27 10:53:21 +0200

    Updated depracated functions

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,33 +3,6 @@ return {
 	dependencies = { { "nvim-lua/plenary.nvim" } },
 	event = "BufRead",
 	opts = {
-		signs = {
-			add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-			change = {
-				hl = "GitSignsChange",
-				text = "▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-			delete = {
-				hl = "GitSignsDelete",
-				text = "▎",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "▎",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-		},
 		numhl = false,
 		linehl = false,
 		watch_gitdir = { interval = 1000 },
