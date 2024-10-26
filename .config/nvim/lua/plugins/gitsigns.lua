2af655811619d8c07a1f6bf78adc53dc07bb6e22
Elia Cunial <elia.cunial@gmail.com>
2024-10-26 11:28:04 +0200

    feat(Neovim): keybinds for git

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -18,6 +18,8 @@ return {
 			{ "<leader>gp", gs.preview_hunk, desc = "Preview hunk" },
 			{ "<leader>gb", gs.toggle_current_line_blame, desc = "Toggle current line blame" },
 			{ "<leader>gs", gs.stage_hunk, desc = "Stage hunk" },
+			{ "<leader>gr", gs.reset_hunk, desc = "Stage Reset Hunk" },
+			{ "<leader>gR", gs.reset_buffer, desc = "Stage Reset Hunk" },
 			{ "<leader>gS", gs.stage_buffer, desc = "Stage buffer" },
 			{ "<leader>gu", gs.undo_stage_hunk, desc = "Undo stage hunk" },
 			{ "]h", gs.next_hunk, desc = "Next Hunk" },
--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,11 +1,19 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
 
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-  },
-  config = true
+		-- Only one of these is needed, not both.
+		"nvim-telescope/telescope.nvim", -- optional
+	},
+	config = true,
+	keys = function()
+		local neogit = require("neogit")
+		local keys = {
+			{ "<leader>g", "", desc = "+Git" },
+			{ "<leader>gn", neogit.open, desc = "Open Neogit" },
+		}
+		return keys
+	end,
 }
