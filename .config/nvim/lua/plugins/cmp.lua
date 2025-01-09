342acf60f5d7b1617b0f0bb3a158c1df19d8f6f8
Elia Cunial <elia.cunial@gmail.com>
2025-01-09 09:18:37 +0100

    Update plugins + adapt blink

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,26 +3,13 @@ return {
 	version = "*",
 	dependencies = { { "rafamadriz/friendly-snippets" }, { "L3MON4D3/LuaSnip", version = "v2.*" } },
 	opts = {
-		snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		},
 		keymap = {
 			-- set to 'none' to disable the 'default' preset
 			preset = "none",
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide" },
 			["<C-CR>"] = { "select_and_accept" },
+			["<C-y>"] = { "select_and_accept" },
 			["<C-p>"] = { "select_prev", "fallback" },
 			["<C-n>"] = { "select_next", "fallback" },
 
@@ -38,8 +25,9 @@ return {
 		},
 
 		signature = { enabled = true },
+		snippets = { preset = "luasnip" },
 		sources = {
-			default = { "lazydev", "luasnip", "lsp", "path", "snippets", "buffer" },
+			default = { "lazydev", "snippets", "lsp", "path", "snippets", "buffer" },
 			providers = {
 				lazydev = {
 					name = "LazyDev",
