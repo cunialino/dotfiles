0fc58970c5333957f9c7d2cfd4895568df013a55
Elia Cunial <elia.cunial@gmail.com>
2025-02-06 15:58:58 +0100

    nvim: fix cmp keybinds

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -17,6 +17,20 @@ return {
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 			["<Tab>"] = { "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
+      cmdline = {
+        preset = "none",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      }
 		},
 
 		appearance = {
@@ -32,7 +46,7 @@ return {
 		signature = { enabled = true },
 		snippets = { preset = "luasnip" },
 		sources = {
-			default = { "lazydev", "snippets", "lsp", "path", "snippets", "buffer" },
+			default = { "lazydev", "snippets", "lsp", "path", "buffer" },
 			providers = {
 				lazydev = {
 					name = "LazyDev",
