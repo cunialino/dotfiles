398024f9d254f5caf8c0e250782a070f57945a7a
Elia Cunial <elia.cunial@gmail.com>
2025-09-03 23:05:26 +0200

    feat(nvim): lsp mapping

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -30,3 +30,18 @@ vim.diagnostic.config({
     },
   },
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.bo[args.buf].formatexpr = nil
+    vim.bo[args.buf].omnifunc = nil
+    vim.keymap.del("n", "K", { buffer = args.buf })
+    vim.keymap.set("n", "<leader>lK", vim.lsp.buf.hover, { buffer = args.buf, desc = "Docs" })
+    vim.keymap.set("n", "<leader>l", "", { buffer = args.buf, desc = "+LSP" })
+
+    vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { buffer = args.buf, desc = "Go to definition" })
+    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { buffer = args.buf, desc = "References" })
+    vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, { buffer = args.buf, desc = "Rename" })
+    vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { buffer = args.buf, desc = "Code actions" })
+  end
+})
