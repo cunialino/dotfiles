69f67b59733781f2b894f976fb5da461ed67e51b
Elia Cunial <elia.cunial@gmail.com>
2025-09-11 18:47:25 +0200

    feat(nvim): need external lock file
    this is not very pretty but it works, I just need to always clone the
    dotfiles in the same path...

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     extraPackages = with pkgs; [ lua-language-server nil nixfmt ];
+    extraLuaConfig = ''
+
+    ${builtins.readFile ./conf/init.lua}
+    '';
 
   };
-  home.file.".config/nvim".source = ./conf;
+  home.file.".config/nvim/lua".source = ./conf/lua;
+  home.file.".config/nvim/lsp".source = ./conf/lsp;
 }
