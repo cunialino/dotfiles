78c86f66af3f89eea48b40bfc9ce90c48dd7d3d4
Elia Cunial <elia.cunial@gmail.com>
2025-09-12 17:27:40 +0200

    feat(fonts): to hm

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,8 +1,33 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    google-chrome
-  ];
+  pkgs,
+  lib,
+  ...
+}:
+{
+  home.packages =
+    with pkgs;
+    [
+      google-chrome
+      noto-fonts-color-emoji
+    ]
+    ++ (with pkgs.nerd-fonts; [ sauce-code-pro ]);
 
   home.file.".local/share/applications/gc.desktop".source = ./google-chrome.desktop;
+
+  fonts.fontconfig = {
+    enable = true;
+
+    defaultFonts = {
+      monospace = [ "Sauce Code Pro Nerd Font" ];
+      sansSerif = [ "Noto Sans" ];
+      serif = [ "Noto Serif" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
+  home.activation = {
+    refresh-font-cache = lib.hm.dag.entryAfter [ "installPackages" ] ''
+      ${pkgs.fontconfig}/bin/fc-cache -f -v
+    '';
+  };
 }
