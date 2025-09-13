ce8a66960e7801aa92bed2b22b088119266aecc0
Elia Cunial <elia.cunial@gmail.com>
2025-09-13 17:46:16 +0200

    feat(git): to hm

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -10,4 +10,39 @@
     gcc
     uv
   ];
+
+  programs.git = {
+    enable = true;
+    userName = "Elia Cunial";
+    userEmail = "elia.cunial@gmail.com";
+
+    difftastic = {
+      enable = true;
+      enableAsDifftool = true;
+      background = "dark";
+      color = "auto";
+      display = "side-by-side";
+    };
+
+    extraConfig = {
+      credential.helper = "cache";
+
+      filter.lfs = {
+        process = "git-lfs filter-process";
+        required = true;
+        clean = "git-lfs clean -- %f";
+        smudge = "git-lfs smudge -- %f";
+      };
+
+      init.defaultBranch = "main";
+    };
+
+    includes = [
+      {
+        path = "~/WORK/.gitconfig";
+        condition = "gitdir:~/WORK/";
+      }
+    ];
+
+  };
 }
