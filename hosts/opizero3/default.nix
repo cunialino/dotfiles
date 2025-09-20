f2b0ce549c46510a8ef09e261030c7d915de31ec
Elia Cunial <elia.cunial@gmail.com>
2025-09-20 12:45:18 +0200

    bu

--- hosts/opizero3/default.nix
+++ hosts/opizero3/default.nix
@@ -7,16 +7,10 @@
 
 let
   # choose kernel package set that has up-to-date DTBs for sun50i-h618
+  kernelPkgs = pkgs.linuxPackages_latest; # change if you want to pin/build a specific kernel
   username = "elia";
   homedir = "/home/${username}";
   stateVersion = "25.05";
-  hostPkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-
-  # cross is the "aarch64" cross-target package set available on the host
-  cross = hostPkgs.pkgsCross.aarch64-multiplatform;
-
-  # choose kernel package set from the cross set (so the host knows how to build/resolve it)
-  kernelPkgsCross = cross.linuxPackages_latest;
 in
 {
   imports = [
@@ -26,23 +20,24 @@ in
   networking.hostName = "opizero3";
   system.stateVersion = stateVersion;
 
-  boot.kernelPackages = kernelPkgsCross;
+  # kernel / firmware
+  boot.kernelPackages = kernelPkgs;
   hardware.enableRedistributableFirmware = true; # if you need Wi-Fi/BT blobs
 
   # build an sdImage and inject the board-specific U-Boot SPL into the raw image
   sdImage = {
     compressImage = false; # easier for these boards and dd placement
+    postBuildCommands = ''
+      # write SPL+u-boot to the sectors expected by sunxi
+      dd if=${pkgs.ubootOrangePiZero3}/u-boot-sunxi-with-spl.bin of=$img bs=1024 seek=8 conv=notrunc
+    '';
   };
 
   users.users.${username} = {
     isNormalUser = true;
-    createHome = true;
+    home = homedir;
     extraGroups = [
       "wheel"
-      "k3s"
     ];
-    home = homedir;
-    shell = pkgs.nushell;
-
   };
 }
