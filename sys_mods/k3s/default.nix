2442d5a1da4a80183a447ca15b3a666d9e699faa
Elia Cunial <elia.cunial@gmail.com>
2025-09-29 20:09:40 +0200

    feat(k3s): registries

--- sys_mods/k3s/default.nix
+++ sys_mods/k3s/default.nix
@@ -12,18 +12,24 @@ let
   server_ip = "192.168.0.2";
   token_file = "/etc/k3s/token";
 
+  extra_flags_common = [
+    "--flannel-iface=${eth}"
+  ];
+
   extra_flags_server = [
     "--write-kubeconfig-mode=640"
     "--write-kubeconfig-group=k3s"
     "--node-ip=${server_ip}"
     "--advertise-address=${server_ip}"
-    "--flannel-iface=${eth}"
-  ];
+    "--disable=traefik"
+
+  ]
+  ++ extra_flags_common;
   registryPort = 5000;
   registryAddr = "${server_ip}:${toString registryPort}";
-  attrToStr = name: "--node-label ${name}=${toString (k3s_labels.${name})}";
+  attrToStr = name: "--node-label=${name}=${toString (k3s_labels.${name})}";
   labels_list = builtins.map attrToStr (builtins.attrNames k3s_labels);
-  extra_flags_agent = [ "--flannel-iface=${eth}" ] ++ labels_list;
+  extra_flags_agent = extra_flags_common ++ labels_list;
 in
 {
 
@@ -32,12 +38,28 @@ in
   networking.firewall = {
 
     interfaces = {
+
+      "cni0" = {
+        allowedTCPPorts = [
+          6443
+          10250
+        ];
+      };
+
       ${eth} = {
         allowedTCPPorts = [
           6443
           10250
         ]
-        ++ (if k3s_role == "server" then [ registryPort ] else [ ]);
+        ++ (
+          if k3s_role == "server" then
+            [
+              registryPort
+              5001
+            ]
+          else
+            [ ]
+        );
         allowedUDPPorts = [ 8472 ];
       };
     };
@@ -47,7 +69,10 @@ in
     mirrors:
       docker.io:
         endpoint:
-          - "http://${registryAddr}"
+          - "http://${server_ip}:5000"
+      ghcr.io:
+        endpoint:
+          - "http://${server_ip}:5001"
   '';
 
   services.k3s = {
@@ -79,13 +104,22 @@ in
         ports = [ "5000:5000" ];
 
         environment = {
-          REGISTRY_VERSION = "0.1";
-          REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY = "/var/lib/registry";
           REGISTRY_HTTP_ADDR = ":5000";
           REGISTRY_PROXY_REMOTEURL = "https://registry-1.docker.io";
-          REGISTRY_PROXY_TTL = "168h";
         };
+        volumes = [
+          "/var/lib/registry:/var/lib/registry:Z"
+        ];
+      };
 
+      ghcrRegistry = {
+        image = "docker.io/library/registry:3";
+        autoStart = true;
+        ports = [ "5001:5000" ];
+        environment = {
+          REGISTRY_HTTP_ADDR = ":5000";
+          REGISTRY_PROXY_REMOTEURL = "https://ghcr.io";
+        };
         volumes = [
           "/var/lib/registry:/var/lib/registry:Z"
         ];
