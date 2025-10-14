2cc93279f7a51dd62031ff1bacc242288cbe1cdd
Elia Cunial <elia.cunial@gmail.com>
2025-10-14 22:00:08 +0200

    feat(nvim): new open window yazi+tmux

--- modules/nvim/conf/lua/core/utils.lua
+++ modules/nvim/conf/lua/core/utils.lua
@@ -6,7 +6,15 @@ local function smart_move(direction, tmux_cmd)
   end
 end
 
-vim.keymap.set('n', '<C-h>', function() smart_move('h', '-L') end, {silent = true})
-vim.keymap.set('n', '<C-j>', function() smart_move('j', '-D') end, {silent = true})
-vim.keymap.set('n', '<C-k>', function() smart_move('k', '-U') end, {silent = true})
-vim.keymap.set('n', '<C-l>', function() smart_move('l', '-R') end, {silent = true})
+vim.keymap.set('n', '<C-h>', function() smart_move('h', '-L') end, { silent = true })
+vim.keymap.set('n', '<C-j>', function() smart_move('j', '-D') end, { silent = true })
+vim.keymap.set('n', '<C-k>', function() smart_move('k', '-U') end, { silent = true })
+vim.keymap.set('n', '<C-l>', function() smart_move('l', '-R') end, { silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>-", "", {
+  callback = function()
+    local dir = vim.fn.expand("%:h")
+    vim.fn.system({ "tmux", "new-window", "-c", dir, "yazi; tmux select-window -t !" })
+  end,
+  desc = "Run yazi in current buffer dir"
+})
