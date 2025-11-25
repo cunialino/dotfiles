3ef661960256480b76ea4c02d017f4d1acd58af6
Elia Cunial <elia.cunial@gmail.com>
2025-11-25 21:21:18 +0100

    feat(nvim): simple statusline

--- modules/nvim/conf/lua/core/statusline.lua
+++ modules/nvim/conf/lua/core/statusline.lua
@@ -1,21 +1,28 @@
-local statusline = {
-  ' %t',
-  '%r',
-  '%m',
-  '%=',
-  '%{&filetype}',
-  ' %2p%%',
-  ' %3l:%-2c '
-}
+local cmp = {}
 
-local function lsp_clients()
+function _G._statusline_component(name)
+  return cmp[name]()
+end
+
+function cmp.lsp_clients()
   local curr_buffer = vim.fn.bufnr()
   local clients = vim.lsp.get_clients({ bufnr = curr_buffer })
   local clients_list = {}
   for _, client in pairs(clients) do
     table.insert(clients_list, client.name)
   end
-  return " :" .. table.concat(clients_list, ", ")
+  return "LSPs: " .. table.concat(clients_list, ", ")
 end
+
+local statusline = {
+  " %t ",
+  "%{%v:lua._statusline_component('lsp_clients')%}",
+  "%r",
+  "%m",
+  "%=",
+  "%{&filetype}",
+  "%3l:%-2c"
+}
+
 
 vim.o.statusline = table.concat(statusline, '')
