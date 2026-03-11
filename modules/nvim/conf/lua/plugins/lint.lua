6384d0fe42fd9e0a00a97cfa5a2d4320b4dc5bdc
Elia Cunial <elia.cunial@gmail.com>
2026-03-11 22:27:43 +0100

    bu

--- modules/nvim/conf/lua/plugins/lint.lua
+++ modules/nvim/conf/lua/plugins/lint.lua
@@ -1,26 +1,35 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = "BufRead",
-	opts = {
-		linters_by_ft = require("tools").tool_by_type("linters"),
-		events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-	},
-	config = function(_, opts)
-		local lint = require("lint")
-		lint.linters_by_ft = opts.linters_by_ft
-		local lintGrp = vim.api.nvim_create_augroup("LintAutogroup", { clear = true })
-		local M = {}
-		function M.lint()
-			local names = lint._resolve_linter_by_ft(vim.bo.filetype)
-			names = vim.list_extend({}, names)
-			if #names == 0 then
-				vim.list_extend(names, lint.linters_by_ft["_"] or {})
-			end
-			vim.list_extend(names, lint.linters_by_ft["*"] or {})
-			if #names > 0 then
-				lint.try_lint(names)
-			end
-		end
-		vim.api.nvim_create_autocmd(opts.events, { callback = M.lint, group = lintGrp })
-	end,
+  "mfussenegger/nvim-lint",
+  event = "BufRead",
+  opts = {
+    linters_by_ft = require("tools").tool_by_type("linters"),
+    linters = {
+      sqlfluff = {
+        args = { "lint", "--format", "json" },
+        stdin = false,
+        ignore_exitcode = true,
+      },
+    },
+    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  },
+  config = function(_, opts)
+    local lint = require("lint")
+    lint.linters_by_ft = opts.linters_by_ft
+    lint.linters.sqlfluff = vim.tbl_deep_extend("force", lint.linters.sqlfluff, opts.linters.sqlfluff or {})
+    local lintGrp = vim.api.nvim_create_augroup("LintAutogroup", { clear = true })
+    local M = {}
+    function M.lint()
+      local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+      names = vim.list_extend({}, names)
+      if #names == 0 then
+        vim.list_extend(names, lint.linters_by_ft["_"] or {})
+      end
+      vim.list_extend(names, lint.linters_by_ft["*"] or {})
+      if #names > 0 then
+        lint.try_lint(names)
+      end
+    end
+
+    vim.api.nvim_create_autocmd(opts.events, { callback = M.lint, group = lintGrp })
+  end,
 }
