c5c9c00efd49e851ad02476f3fbe41a2fd3d10ee
Elia Cunial <elia.cunial@gmail.com>
2026-03-15 22:54:58 +0100

    feat: rotate keys

--- hosts/hp_1/default.nix
+++ hosts/hp_1/default.nix
@@ -35,7 +35,7 @@ in
 
     users.users.${username} = {
       openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQClmGcdVOIKxGE09mbmqc/RpLgfGnuWIM8jd8ShoSQyFDBnpiorURYOjs14aGw1hp0KPX0jYdN6fd/xoYdCCC0e2GshJnwlkN1OjXfMTjzIqWNa7oSoL6lhBSp22Aq+pY2kk7gL1j8q4WUTBNUcDPQVmnQdZGmhXgNSSNX6RChkEKhmAKtgLsO6FAZL4JcU4uG1o7HGtn9FN2DmYje/J9Q+WFgyYhz03TzCiyLmwsgYuM3lSwT999TztQ8tQtrVyrJcLpuA4gN0KrHidvWGit25m/bR/FD0lcHKkaxkKhBreaBYWzOKA7H8cZr9qdW9zjv9cnadXV92aQde5XZN6tXgf+N3jys2VIXLKkkKFVZ3IuHm6ZLsgY74lCzXmXDFpdzY3zR2qibqyubEBTHnXb04yb3Tgyl13RT/Vq8Sa8c+z2L8eu5o3BeyGu2acYFnn7u+AtSR4HNU5Pi1DPL8k65Us440ywurYfXwZ8qGknNNydEbZDXR+hcDK8RiTWohaWcy9L5jiRpSbgacEAHbm2Nl9BUx6muvjKiOFwhdbw3QBThCqNmSO6JcTLmIZt/uutw2vBwCKTs6i/jImVjPUIIe3vbFviCIsW4MENJglOZbvvfLGZsVXzWF/Wdmv5VAybsoom8iIKhwgZlwsJDzjRl3Yo4QB6ZA3225/p7o/fIsAw== elia.cunial@gmail.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPKPt5/R03Ivma94RuLK4e6vwgwiQbV/jMcvkVbpqGT elia@elcungem"
       ];
     };
 
