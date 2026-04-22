0a0a8de602e44cf75b1e044726d0f57cdd9bf5f4
Elia Cunial <elia.cunial@gmail.com>
2026-04-22 09:22:09 +0200

    fix(orgmac): ssl certs

--- hosts/orgmac.nix
+++ hosts/orgmac.nix
@@ -22,9 +22,6 @@
       awscli2
     ];
     home.sessionVariables = {
-      SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-      REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
-      CURL_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
       TMPDIR="/home/d00f192.linux/tmp";
     };
     home.username = "d00f192";
