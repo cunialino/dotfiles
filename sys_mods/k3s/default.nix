8dba725b76149d454399315c9f9bc3292f4a92b7
Elia Cunial <elia.cunial@gmail.com>
2026-07-31 11:57:19 +0200

    chore(k3s): separate kube-vip

--- sys_mods/k3s/default.nix
+++ sys_mods/k3s/default.nix
@@ -20,66 +20,10 @@ let
   };
 
   # Template the DaemonSet based on official documentation logic
-  kubeVipManifest = pkgs.writeText "kube-vip.yaml" ''
-    apiVersion: v1
-    kind: Pod
-    metadata:
-      creationTimestamp: null
-      name: kube-vip
-      namespace: kube-system
-    spec:
-      containers:
-      - args:
-        - manager
-        env:
-        - name: vip_arp
-          value: "true"
-        - name: port
-          value: "6443"
-        - name: vip_interface
-          value: "${cfg.eth}"
-        - name: cp_enable
-          value: "true"
-        - name: cp_namespace
-          value: kube-system
-        - name: vip_ddns
-          value: "false"
-        - name: svc_enable
-          value: "true"
-        - name: vip_leaderelection
-          value: "true"
-        - name: vip_leaseduration
-          value: "5"
-        - name: vip_renewdeadline
-          value: "3"
-        - name: vip_retryperiod
-          value: "1"
-        - name: address
-          value: "${cfg.kube_vip_ip}"
-        image: ghcr.io/kube-vip/kube-vip:v1.2.0
-        imagePullPolicy: Always
-        name: kube-vip
-        resources: {}
-        securityContext:
-          capabilities:
-            add:
-            - NET_ADMIN
-            - NET_RAW
-            - SYS_TIME
-        volumeMounts:
-        - mountPath: /etc/kubernetes/admin.conf
-          name: kubeconfig
-      hostAliases:
-      - hostnames:
-        - kubernetes
-        ip: 127.0.0.1
-      hostNetwork: true
-      volumes:
-      - hostPath:
-          path: /etc/rancher/k3s/k3s.yaml
-        name: kubeconfig
-    status: {}
-  '';
+  kubeVipManifest = pkgs.writeText "kube-vip.yaml" (lib.replaceStrings
+    [ "{{ETH}}" "{{KUBE_VIP_IP}}" ]
+    [ cfg.eth cfg.kube_vip_ip ]
+    (builtins.readFile ./kube-vip.yaml));
 in
 {
 
